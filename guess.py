10a2bb4d67f8de9cecff9ea3cf014f5237c1d60a
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2019-05-30 20:01:23 -0400

    Guess.py comments

--- guess.py
+++ guess.py
@@ -1,8 +1,11 @@
+# Used to generate random number
 import random
+
 def guess():
     mynum = random.randint(1, 10)
     usernum = 0
     numguess = 0
+    # Do this until user gets it right or they max out guesses
     while (usernum != mynum) and (numguess < 3):
         print("Please enter a number between 1 and 10:")
         usernum = int(input())
@@ -13,6 +16,7 @@ def guess():
             print("Your number is too low")
         else:
             print("You got it right!")
+    # If we broke out of the loop and the numbers aren't the same we know they ran out of guesses
     if usernum != mynum:
         print("You ran out of guesses!")
 
